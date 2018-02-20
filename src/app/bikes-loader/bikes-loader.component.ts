658d489cbac3241f419fd994aeef0ac0245a5dd3
Andrey Kravtsov <an9949an@gmail.com>
2018-02-20 19:37:39 +0300

    bikes loader

---
+++ src/app/bikes-loader/bikes-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bikes-loader',
+  templateUrl: 'bikes-loader.component.html',
+  styleUrls: [ 'bikes-loader.component.scss' ],
+})
+
+export class BikesLoaderComponent implements OnInit {
+  constructor() {
+  }
+
+  public ngOnInit() {
+  }
+}
---
+++ src/app/bikes-loader/index.ts
@@ -0,0 +1,1 @@
+export * from './bikes-loader.component';
