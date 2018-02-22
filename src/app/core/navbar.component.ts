9a93edca0df1bd92eb71aa91f67a516c7f31cac1
Andrey Kravtsov <an9949an@gmail.com>
2018-02-22 14:43:54 +0300

    navbar

--- src/app/core/navbar.component.ts
+++ src/app/core/navbar.component.ts
@@ -4,20 +4,40 @@ import { Component, OnInit } from '@angular/core';
   selector: 'navbar',
   styleUrls: [ './navbar.component.scss' ],
   template: `
-    <nav>
-      <a [routerLink]=" ['./'] " mat-button
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Загрузчик товаров
-      </a>
-      <!--<a [routerLink]=" ['./home'] " mat-button-->
-        <!--routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">-->
-        <!--Home-->
-      <!--</a>-->
+
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse navbar-fixed-top">
+      <button class="navbar-toggler navbar-toggler-right" type="button"
+              (click)="isCollapsed = !isCollapsed"
+              [attr.aria-expanded]="!isCollapsed" aria-controls="navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" [routerLink]=" ['./'] " >Veloman Crm</a>
+      <div class="collapse navbar-collapse" id="navbarNav" [ngbCollapse]="isCollapsed">
+        <ul class="navbar-nav">
+          <li class="nav-item" routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+            <a class="nav-link" [routerLink]=" ['./'] ">
+              Импорт товаров
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Менеджер прайс-листов
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
+    
+    
+    
+    
     `
 })
 
 export class NavbarComponent implements OnInit {
+  public isCollapsed = true;
+
   public ngOnInit() {
   }
 }
