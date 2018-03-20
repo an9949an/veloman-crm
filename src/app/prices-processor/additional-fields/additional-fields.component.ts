b5b24053868340837a2a3763ae14976f0b3ca816
Andrey Kravtsov <an9949an@gmail.com>
2018-03-20 18:15:47 +0300

    additional fields

--- src/app/prices-processor/additional-fields/additional-fields.component.ts
+++ src/app/prices-processor/additional-fields/additional-fields.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as Cookies from 'cookies-js';
 
 @Component({
   selector: 'additional-fields',
   templateUrl: 'additional-fields.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdditionalFieldsComponent {
+export class AdditionalFieldsComponent implements OnInit {
+
+  private static readonly DRAFT_COOKIE = 'AdditionalFieldsDraft';
 
   public form: FormGroup;
   @Output() public buildFile = new EventEmitter();
@@ -23,12 +26,31 @@ export class AdditionalFieldsComponent {
       deliveryBelarusTime: ['', Validators.required],
       deliveryBelarusPrice: ['', Validators.required],
       lifeTime: ['', Validators.required],
-      forblablablaonly: ['', Validators.required],
-      credit: ['', Validators.required],
+      forblablablaonly: [''],
+      credit: [''],
 
       updateOldItems: [false],
       deleteRedundant: [false]
     });
+  }
+
+  /**
+   * ngOnInit
+   */
+  public ngOnInit() {
+
+    const draft = Cookies.get(AdditionalFieldsComponent.DRAFT_COOKIE);
+
+    if (draft) {
+      this.form.setValue(JSON.parse(draft));
+    }
+
+    this.form.valueChanges
+      .do((value) => Cookies.set(
+        AdditionalFieldsComponent.DRAFT_COOKIE,
+        JSON.stringify(value)))
+      .subscribe();
+
   }
 
 }
